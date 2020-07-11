68f4484f2db134869f1d2b3837f5a939e4984b1d
DESKTOP-4G0SSUV\user <x890311x@gmail.com>
2020-07-11 19:09:29 +0800

    add code and complete downloader

--- api/douyin_tools.py
+++ api/douyin_tools.py
@@ -1,1 +1,17 @@
-import  requests
+import requests
+import re
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+api = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={video_id}"
+def download_video(url:str):
+    req=requests.get(url,headers)
+    id=re.findall(r"video/(\d+)",req.url)[0]
+    # print(video_id)
+    req_api=requests.get(api.format(video_id=id),headers)
+    api_json=req_api.json()
+    video_download_url = api_json['item_list'][0]['video']["play_addr"]["url_list"][0]
+    copywriting = api_json['item_list'][0]['share_info']['share_title']
+    print(video_download_url,copywriting)
+    req_video = requests.get(video_download_url, headers)
+    with open('{}.mp4'.format(str(copywriting)), 'wb') as f:
+        f.write(req_video.content)
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import re
 from api import douyin_tools
+
+def parse_url(url:str)->list:
+    urls = re.findall(r"https://v\.douyin\.com/.*/",url)
+    return urls
+
+if __name__ == "__main__":
+    while 1:
+    # if __name__ == "__main__":
+        user_input=input('input url')
+        if user_input=='':
+            exit()
+            # break
+        urls = parse_url(user_input)
+        for i in urls:
+            douyin_tools.download_video(i)
